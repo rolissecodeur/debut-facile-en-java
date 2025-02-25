51a2f42328e25dd60d2c18694f4f6026ae4a7c73
rolissecodeur <rolissecodeur@gmail.com>
2025-02-25 14:40:38 +0100

    p1c2d2ok

--- src/com/zerofiltre/parkingbot/Main.java
+++ src/com/zerofiltre/parkingbot/Main.java
@@ -7,7 +7,9 @@ public class Main {
        int length = 180;
        int width = 120;
        int surface = length * width;
+       int perimeter = (length + width)*2;
        System.out.println("La surface au sol de la voiture est de: "+ surface);
+       System.out.println("Le perimmètre au sol de la voiture est de: "+ perimeter);
 
     }
 }
