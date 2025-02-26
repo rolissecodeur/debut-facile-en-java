3a817057fe6b37d84aa197d1a895eab7e74f889e
rolissecodeur <rolissecodeur@gmail.com>
2025-02-26 10:11:07 +0100

    p1c4ok

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -1,7 +1,11 @@
 package com.zerofiltre.parkingbot;
 
+import com.zerofiltre.parkingbot.service.ParkingService;
+
 public class ParkingBot {
 
+  static ParkingService parkingService = new ParkingService();
+static String completion = " des équipes du parking zerofiltre";
   /**
    * Ceci est la méthode Main
    *
@@ -17,11 +21,15 @@ public class ParkingBot {
    * Cette méthode permet de dire Hello
    */
   private static void sayHello() {
-    String welcomeSentence = "Hello, recevez la bienvenue ";
+    String welcomeSentence = "Hello, recevez la bienvenue" + completion;
     System.out.println(welcomeSentence);
+    String parkService = parkingService.parking;
+    String washingService = parkingService.washing;
+
+    System.out.println("Nous proposons les services suivants : "+ parkService + " ET " + washingService);
   }
 
   private static void sayBye() {
-    System.out.println("Recevez les aurevoirs");
+    System.out.println("Recevez les aurevoirs" + completion);
   }
 }
---
+++ src/com/zerofiltre/parkingbot/service/ParkingService.java
@@ -0,0 +1,6 @@
+package com.zerofiltre.parkingbot.service;
+
+public class ParkingService {
+  public  String parking = "Parking et surveillance de véhicule";
+  public  String washing = "laverie  de véhicule";
+}
