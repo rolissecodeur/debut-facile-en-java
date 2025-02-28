49717992aa7864e12264037737337e2980135f0b
rolissecodeur <rolissecodeur@gmail.com>
2025-02-28 16:18:51 +0100

    p1c5ok

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -15,6 +15,32 @@ public class ParkingBot {
    * @param args : Tableau de données entrées lors du lancement de l'application
    */
   public static void main(String[] args) {
+    /*for (int i = 0; i < customers.length; i++) {
+      sayHello(customers[i]);
+    }*/
+
+    /*for(String customer:customers){
+      sayHello(customer);
+    }*/
+
+    System.out.println("Nous avons "+ parkingSize + " place(s) restante(s)");
+
+    int customerNumber = 0 ;
+
+    while(parkingSize>0){
+      parkingSize--;
+      if("Réné".equals(customers[customerNumber])){
+        customerNumber++;
+        parkingSize++;
+        continue;
+      }
+
+      if("Booba".equals(customers[customerNumber])){
+        break;
+      }
+      sayHello(customers[customerNumber]);
+      customerNumber++;
+    }
 
   }
 
