f586bba1ec3579eb3d8481f58d8908d0164239e6
rolissecodeur <rolissecodeur@gmail.com>
2025-03-03 09:45:26 +0100

    p1c6ok

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -1,5 +1,9 @@
 package com.zerofiltre.parkingbot;
 
+import com.zerofiltre.parkingbot.model.Ticket;
+import com.zerofiltre.parkingbot.model.Vehicle;
+import com.zerofiltre.parkingbot.service.ParkingService;
+
 public class ParkingBot {
 
 
@@ -9,6 +13,14 @@ public class ParkingBot {
    * @param args : Tableau de données entrées lors du lancement de l'application
    */
   public static void main(String[] args) {
+    Vehicle vehicle = new Vehicle();
+    vehicle.setRegistrationNumber("LS-324-PM");
+    vehicle.setCategory("FORD");
+
+    ParkingService parkingService = new ParkingService();
+    Ticket ticket = parkingService.processIncommingVehicle(vehicle);
+
+    System.out.println(ticket);
   }
 
 
---
+++ src/com/zerofiltre/parkingbot/service/ParkingService.java
@@ -0,0 +1,17 @@
+package com.zerofiltre.parkingbot.service;
+
+import com.zerofiltre.parkingbot.model.Ticket;
+import com.zerofiltre.parkingbot.model.Vehicle;
+
+import java.util.Date;
+
+public class ParkingService {
+    public Ticket processIncommingVehicle(Vehicle vehicle){
+        Ticket ticket = new Ticket();
+        Date now = new Date();
+        ticket.setEnteringTime(now);
+        ticket.setVehicle(vehicle);
+
+        return ticket;
+    }
+}
